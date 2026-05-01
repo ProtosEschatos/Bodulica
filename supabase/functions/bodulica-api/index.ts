2fe4ddfd824feb27d27e4d5be2e924f4db043d71
Sovereign <sovereign@Protos.localdomain>
2026-05-01 16:47:29 +0200

    chore: dodan @ts-nocheck za IDE kompatibilnost

--- supabase/functions/bodulica-api/index.ts
+++ supabase/functions/bodulica-api/index.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+// Deno Edge Function - runs in Supabase Edge Runtime
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
